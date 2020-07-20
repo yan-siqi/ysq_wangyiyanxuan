81954441fa88dd96d1ca7597e957d0ee9b417fa4
yansiqi <1749787628@qq.com>
2020-07-20 22:02:12 +0800

    更新

--- src/components/index/Content/Content.vue
+++ src/components/index/Content/Content.vue
@@ -5,43 +5,39 @@
     <div class="content">
       <el-carousel trigger="click" height="150px" class="carsoulContainer">
         <el-carousel-item v-for="item in 4" :key="item">
-          <img src="https://yanxuan.nosdn.127.net/99873ee03dbfa1fec94f273025baaf20.jpg?type=webp&imageView&quality=75&thumbnail=750x0" alt="">
+          <img
+            src="https://yanxuan.nosdn.127.net/99873ee03dbfa1fec94f273025baaf20.jpg?type=webp&imageView&quality=75&thumbnail=750x0"
+            alt=""
+          />
           <h3 class="small">{{ item }}</h3>
         </el-carousel-item>
       </el-carousel>
-      <!--   <div class="swiper-container" ref="swiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <img
-              src="https://yanxuan.nosdn.127.net/99873ee03dbfa1fec94f273025baaf20.jpg?type=webp&imageView&quality=75&thumbnail=750x0"
-              alt=""
-              srcset=""
-            />
-          </div>
-          <div class="swiper-slide">
-            <img
-              src="https://yanxuan.nosdn.127.net/50bc9d8901b05f7917156584812a853f.jpg?type=webp&imageView&quality=75&thumbnail=750x0"
-              alt=""
-              srcset=""
-            />
-          </div>
-          <div class="swiper-slide">
-            <img
-              src="https://yanxuan.nosdn.127.net/50bc9d8901b05f7917156584812a853f.jpg?type=webp&imageView&quality=75&thumbnail=750x0"
-              alt=""
-              srcset=""
-            />
-          </div>
-        </div>
-        <div class="swiper-slide-paginnation-bullets">
-          <span class="swiper-paginnation-bullet"></span>
-          <span class="swiper-paginnation-bullet"></span>
-          <span class="swiper-paginnation-bullet"></span>
-          <span class="swiper-paginnation-bullet"></span>
-          <span class="swiper-paginnation-bullet"></span>
-          <span class="swiper-paginnation-bullet"></span>
-        </div>
-      </div> -->
+      <div class="indexService">
+        <ul class="g-grow">
+          <li class="item">
+            <a href="">
+              <i class="one"></i>
+              <span class="text">网易自营品牌</span>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <i class="two"></i>
+              <span class="text">30天无忧退货</span>
+            </a>
+          </li>
+
+          <li class="item">
+            <a href="">
+              <i class="three"></i>
+              <span class="text">48小时快速退款</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="listContainer">
+
+      </div>
     </div>
   </div>
 </template>
@@ -60,8 +56,6 @@ export default {
 @basewidth: 37.5rem;
 .container {
   width: 100%;
-  //position: relative;
-  //overflow: hidden;
   .content {
     width: 100%;
     height: calc(100vh - 1.4rem);
@@ -72,7 +66,7 @@ export default {
       left: 0;
       top: 89 / @basewidth;
       width: 100%;
-     // height: 10rem;
+      // height: 10rem;
       background-color: brown;
       .el-carousel__item {
         h3 {
@@ -82,43 +76,65 @@ export default {
           line-height: 150px;
           margin: 0;
         }
-        img{
-          width: 100%;
-          height: 100%;
-        }
-      }
-
-      .el-carousel__item:nth-child(2n) {
-        background-color: #99a9bf;
-      }
-
-      .el-carousel__item:nth-child(2n + 1) {
-        background-color: #d3dce6;
-      }
-    }
-    /* .swiper-container {
-      width: 100%;
-      height: 8rem;
-      position: absolute;
-      left: 0;
-      top: 89 / @basewidth;
-      .swiper-slide {
-        width: 100%;
-        height: 4rem;
-        background-color: blueviolet;
         img {
           width: 100%;
           height: 100%;
         }
       }
-      .swiper-slide-paginnation-bullets {
-        position: absolute;
-        left: 50%;
-        bottom: 0.4rem;
-        width: 100%;
-        text-align: center;
+    }
+    .indexService {
+      width: 100%;
+      height: 36 / @basewidth;
+      position: absolute;
+      left: 0;
+      top: 239 / @basewidth;
+      background-color: brown;
+
+      .g-grow {
+        width: 90%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        background-color: blueviolet;
+        .item {
+          width: 33.33%;
+          height: 100%;
+          .text {
+            font-size: 0.32rem;
+            color: #333;
+            margin-left: 0.10667rem;
+            line-height: 36 / @basewidth;
+            display: inline-block;
+            vertical-align: middle;
+          }
+          i {
+            display: inline-block;
+            vertical-align: middle;
+            //background-image: url(//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/servicePolicy-index-bf1239c157.png);
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            width: 0.42667rem;
+            height: 0.42667rem;
+          }
+          .one {
+            background-image: url("https://yanxuan.nosdn.127.net/a03dd909803b9ac032eba58b7253a2f6.png");
+          }
+          .two {
+            background-image: url(//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/servicePolicy-index-bf1239c157.png);
+          }
+          .three {
+            background-image:url("https://yanxuan.nosdn.127.net/eb61ee48e8942dbd1784c9ee75ebe955.png");
+          }
+        }
       }
-    } */
+    }
+    .listContainer{
+      width: 100%;
+      height: 170/@basewidth;
+      background-color: chartreuse;
+    }
   }
 }
 </style>
