1ac413515ce035c8f32a42f7d1960eff58a904d9
yansiqi <1749787628@qq.com>
2020-07-23 23:11:03 +0800

    购物车界面

--- src/pages/buy/buy.vue
+++ src/pages/buy/buy.vue
@@ -21,9 +21,102 @@
     </div>
     <!-- 内容区 -->
     <div class="content">
-      <!--  <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
-        <li v-for="i in count" class="infinite-list-item">{{ i }}</li>
-      </ul> -->
+      <div class="swiperTabTitle">
+        <div class="topTabTitle">
+          <img
+            src="https://m.you.163.com/topic/index/img/topic_logo.c2284970.png"
+            class="zhidemai"
+            alt=""
+          />
+          <div class="zhidemaiTxt">严选好物 用心生活</div>
+        </div>
+        <img
+          src="https://m.you.163.com/topic/index/img/topic_title_bg.2373a140.png"
+          class="titleImg"
+          alt=""
+        />
+        <div class="swiperTabContainer">
+          <div class="uItem">
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/e21381b7e1e7caebbe72488db9542163.gif"
+                alt=""
+              />
+              <div class="uItemTabTitle">9.9超值</div>
+              <div class="uItemTabTxt">爆品定价直降</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/98f47f438097516c3c4b09adce6f486d.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">员工精选</div>
+              <div class="uItemTabTxt">每天一款内部价</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/e92a567f96b94dcea29eafea9ab42440.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">好物大赏</div>
+              <div class="uItemTabTxt">官方排名发布</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/1d94b7dcbf10a3e836bab65729eeaaad.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">明星商品</div>
+              <div class="uItemTabTxt">百万粉丝之选</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/74db07fdd266ea7282a55f4b1aeaf74c.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">每月好物</div>
+              <div class="uItemTabTxt">当季必买清单</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/9d8114ce4ef435ceac5e82120efeec4c.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">晒单</div>
+              <div class="uItemTabTxt">入选就有红包</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/84395807297dbc88f6569e16960695f1.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">甄选家</div>
+              <div class="uItemTabTxt">爆品免费试用</div>
+            </a>
+            <a href="" class="uItemTab">
+              <img
+                src="https://yanxuan.nosdn.127.net/d98cef378330167ef79c5f956814836e.png?imageView&quality=65&thumbnail=120y120"
+                alt=""
+              />
+              <div class="uItemTabTitle">选购指南</div>
+              <div class="uItemTabTxt">破解选择困难</div>
+            </a>
+          </div>
+          <div class="swiperTabContainerFooter">
+            <div class="containerFooter">
+              <span class="swiper-pagination-bullet active"></span>
+              <span class="swiper-pagination-bullet"></span>
+              <span class="swiper-pagination-bullet"></span>
+              <span class="swiper-pagination-bullet"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
+        <div class="contianerList">
+          <li v-for="i in count" class="infinite-list-item">{{ i }}</li>
+        </div>
+      </ul>
     </div>
   </div>
 </template>
@@ -35,7 +128,7 @@ import "@nutui/nutui/dist/nutui.css";
 import { TabPanel } from "@nutui/nutui";
 NutUI.install(Vue);
 export default {
-  /*   data() {
+  data() {
     return {
       data: new Array(30),
       page: 5,
@@ -62,7 +155,7 @@ export default {
   },
   destroyed() {
     clearTimeout(this.timer);
-  }, */
+  },
 
   data() {
     return {
@@ -83,7 +176,7 @@ export default {
   .top {
     width: 100%;
     height: 50 / @basewidth;
-   // background-color: pink;
+    // background-color: pink;
     .contentTop {
       width: 92%;
       margin: 0 auto;
@@ -106,12 +199,12 @@ export default {
       }
       .topTitle {
         line-height: 50 / @basewidth;
-        font-size: 22 / @basewidth;
+        font-size: 18 / @basewidth;
       }
       .contentRight {
         width: 70 / @basewidth;
         height: 100%;
-       // background-color: yellowgreen;
+        // background-color: yellowgreen;
         display: flex;
         justify-content: space-between;
         .search {
@@ -121,9 +214,9 @@ export default {
             background-position: center;
             background-repeat: no-repeat;
             background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuBAMAAACllzYEAAAAKlBMVEUAAAA0NDQ0NDQ0NDQ0NDQ0NDQzMzMUFBQ0NDROTk4zMzMzMzMzMzM2NDTc2xHNAAAADXRSTlMAbezXT6YsFM0O+L+FggY9rwAAAP5JREFUOMtjIBOwiTat0ghMQBeeaHsXBC5Logoz6l4KTGcoE9W9JIBiSO9tBzCDZe8NZKOcLgOFIRK2KghhlrWRcPbUWw5wttcdJL1nl8BN1z2AJM5zCWYDx1UUt8U2QBmylijiky9CaE5dBxRxlksTIH66jeb3vRC/MV9BE/c1AFO5CmjiTNcg1gagibNCLI4VQBNnhLi7NwE9Km6AKd0CNHH2S2BqLUbc3cIljsscXPbicicuf+EKB1zhhiuc8cYLZjzij3fMdII9XV3Bng7vXi7Amm4Vbc2xp/NisAbMfMEO0YCZj8AaMPMdTAMmQGhAAPwabmAXZ+9mIBUAAIJuXNa9gIYoAAAAAElFTkSuQmCC);
-            background-size:  23 / @basewidth 23 / @basewidth;
+            background-size: 23 / @basewidth 23 / @basewidth;
             width: 31 / @basewidth;
-            height:50 / @basewidth;
+            height: 50 / @basewidth;
           }
         }
         .cart {
@@ -133,9 +226,9 @@ export default {
             background-position: center;
             background-repeat: no-repeat;
             background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAk1BMVEUAAAAzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzM1idyXAAAAMHRSTlMAYbmIKS7ss/BPwREDeAjlt52EIc3HQTs3JRv53djTjn5XFQ2+p6JH9K+XZDJzb12hgxUyAAABlElEQVRIx82V23KCMBCGV8AYAgLlrChaz4ce/vd/utopKks10YvO9LvaMN+E7OZnoP9FCGDgPKzv63SC1KPHWWQ40hMcMXtG32JTHHtGkkbvV1EEM1mje5n8eLf1xLYANRzwSkamu3P1gdhoFyI9ly4GgUn/rNbnsi/DkUkfXk/gZchN+gt6l/pwqg2sUFzqd9gG20+R0LXXaKnXk4kYXxZ9Yeo1l9k1tkuFQq/3sAlafZvuNcbc51PVsmH7ubD0vVooWysnlNpegxROe6lQ6vRR2Jqjude8Uh5PkK0P2EtArNf92rqP6LzcCScSGuYBj5CC69xn5BNnDZeeYA7bf0J3qxXXjYHO+/dIfu+U4T5VvOgeRqC2UkCsB20UIL7HvvKYPQ4xW9Byjqnf+SzqnKioMQv4LUdNdmZsFynKn69zN2IRa8ZewmKXjdVPEeHtth5xXd3Uh82um85hhCzoxJuQCftBTXAYkxdDsMf+qfct0XYKm2dsu8NU1cAnMZYDSKUELI845R6AlVMHbygAOVzQn/MFg0s1dXIHznQAAAAASUVORK5CYII=);
-            background-size:  23 / @basewidth 23 / @basewidth;
-             width: 24.5 / @basewidth;
-            height:50 / @basewidth;
+            background-size: 23 / @basewidth 23 / @basewidth;
+            width: 24.5 / @basewidth;
+            height: 50 / @basewidth;
           }
         }
       }
@@ -144,8 +237,116 @@ export default {
   .content {
     width: 100%;
     height: calc(100vh - 2.64rem);
-    background-color: aquamarine;
     overflow: scroll;
+    .swiperTabTitle {
+      width: 100%;
+      position: relative;
+      .topTabTitle {
+        width: 200 / @basewidth;
+        height: 35 / @basewidth;
+        position: absolute;
+        left: 10 / @basewidth;
+        top: 30 / @basewidth;
+        display: flex;
+        .zhidemai {
+          width: 65 / @basewidth;
+          height: 34.33 / @basewidth;
+        }
+        .zhidemaiTxt {
+          color: white;
+          text-align: center;
+          display: inline-block;
+          height: 34.33 / @basewidth;
+          line-height: 34.33 / @basewidth;
+        }
+      }
+      .titleImg {
+        width: 100%;
+        height: 259 / @basewidth;
+        margin-bottom: 90px;
+      }
+      .swiperTabContainer {
+        width: 94%;
+        height: 270 / @basewidth;
+        background-color: white;
+        margin: 0 auto;
+        position: absolute;
+        left: 3%;
+        top: 70px;
+        border-radius: 10px;
+        .uItem {
+          width: 96%;
+          margin: 0 auto;
+
+          .uItemTab {
+            float: left;
+            display: block;
+            margin-top: 15 / @basewidth;
+            width: 84 / @basewidth;
+            height: 100.5 / @basewidth;
+            //background-color: red;
+            img {
+              display: block;
+              width: 60 / @basewidth;
+              height: 60 / @basewidth;
+              margin: 0 auto;
+            }
+            .uItemTabTitle {
+              font-family: "PingFang-SC-Bold";
+              font-weight: bold;
+              font-size: 14 / @basewidth;
+              white-space: nowrap;
+              color: #333333;
+              text-align: center;
+            }
+            .uItemTabTxt {
+              font-family: "PingFangSC-Regular";
+              font-size: 0.2rem;
+              color: #999999;
+              text-align: center;
+              overflow: hidden;
+            }
+          }
+        }
+        .swiperTabContainerFooter {
+          height: 16.8 / @basewidth;
+          width: 98%;
+          margin: 0 auto;
+          // background-color: yellowgreen;
+          position: absolute;
+          left: 1%;
+          bottom: 3%;
+
+          .containerFooter {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            bottom: 5px;
+            .swiper-pagination-bullet {
+              width: 0.8rem;
+              height: 0.04rem;
+              background: #d9d9d9;
+              margin: 0 !important;
+              border-radius: 0;
+              opacity: 1;
+              display: block;
+              float: left;
+              &.active {
+                background-color: tomato;
+              }
+            }
+          }
+        }
+      }
+    }
+    .infinite-list {
+      background-color: pink;
+      .contianerList {
+        width: 94%;
+        margin: 0 auto;
+        background-color: red;
+      }
+    }
   }
 }
 </style>
--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,13 +1,161 @@
 <template>
-  <div>这里是购物车界面</div>
+  <div class="container">
+    <div class="top">
+      <div class="topTitle">购物车</div>
+      <div class="right">
+        <div class="coupon">
+          <span class="palceholder">领券</span>
+        </div>
+      </div>
+    </div>
+    <div class="titles">
+      <ul class="servicePolicy">
+        <li class="item">
+          <i class="icon"></i>
+          <span>30天无理由退货</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span>48小时快速退款</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span>满99元免邮费</span>
+        </li>
+      </ul>
+    </div>
+    <div class="defaultPage">
+      <div class="cotainer">
+        <div class="img"></div>
+        <div class="txt">去添加点什么吧</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Cart',
-}
+  name: "Cart",
+};
 </script>
 
 <style lang="less" scoped>
+@basewidth: 37.5rem;
+.container {
+  width: 100%;
+  height: calc(100vh - 1.3rem);
+  //background-color: pink;
+  .top {
+    width: 94%;
+    margin: 0 auto;
+    height: 44 / @basewidth;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    .topTitle {
+      line-height: 44 / @basewidth;
+      font-size: 21 / @basewidth;
+    }
+    .palceholder {
+      width: 32 / @basewidth;
+      height: 20 / @basewidth;
+      line-height: 20 / @basewidth;
+      display: block;
+      background-color: orangered;
+      font-size: 11 / @basewidth;
+      color: white;
+      text-align: center;
+      margin-top: 13 / @basewidth;
+      margin-right: 15 / @basewidth;
+      position: relative;
+      &::before {
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        content: "";
+        background-color: white;
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%) translateX(-60%);
+      }
+      &::after {
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        content: "";
+        background-color: white;
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%) translateX(60%);
+      }
+    }
+  }
+  .titles {
+    height: 35 / @basewidth;
+    width: 100%;
+    background-color: #eee;
 
+    .servicePolicy {
+      width: 94%;
+      height: 100%;
+      margin: 0 auto;
+      //background-color: yellow;
+      display: flex;
+      .item {
+        //width: 27%;
+        height: 100%;
+        margin-right: 8.6%;
+        //background-color: yellowgreen;
+        .icon {
+          display: inline-block;
+          width: 3 / @basewidth;
+          height: 3 / @basewidth;
+          border-radius: 50%;
+          border: 1 / @basewidth solid #999;
+        }
+        span {
+          discolor: #666;
+          font-size: 12px;
+          line-height: 35 / @basewidth;
+        }
+      }
+    }
+    
+  }
+  .defaultPage {
+      position: relative;
+      width: 100%;
+      height: calc(100vh - 3.6rem);
+      background-color:#eee;
+      .cotainer {
+        position: absolute;
+        top: -0.66667rem;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        height: 4rem;
+        margin: auto;
+        .img {
+          display: block;
+          // vertical-align: middle;
+          width: 116.39 / @basewidth;
+          height: 116.39 / @basewidth;
+          margin-bottom: 0.10667rem;
+          margin: auto;
+          background-size: 3.30667rem 3.30667rem;
+          background-position: center center;
+          background-repeat: no-repeat;
+          background-image: url(//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/noCart-d6193bd6e4.png?imageView&type=webp);
+        }
+        .txt {
+          font-size: 0.37333rem;
+          line-height: 1;
+          color: #7f7f7f;
+          text-align: center;
+        }
+      }
+    }
+}
 </style>
